2e125b4a4daf11ca7b38c5410397ab3e575489fc
nick udell <nick@udellgames.com>
2022-07-04 15:12:28 +0100

    Removes unneeded template items

--- DungeonFactory/Program.cs
+++ DungeonFactory/Program.cs
@@ -1,4 +1,3 @@
-using DungeonFactory.Data;
 using DungeonFactory.Infrastructure;
 using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Components.Web;
@@ -9,7 +8,6 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddRazorPages();
 builder.Services.AddServerSideBlazor();
-builder.Services.AddSingleton<WeatherForecastService>();
 builder.Services.Configure<DatabaseOptions>(builder.Configuration.GetSection("Database"));
 builder.Services.AddDatabase();
 
