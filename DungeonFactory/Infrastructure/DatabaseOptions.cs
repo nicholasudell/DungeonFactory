19709bdaed38e4fe7d618b5fa43452cb6f4432d7
nick udell <nick@udellgames.com>
2022-07-10 13:29:28 +0100

    Adds a few extra config options for LiteDB

--- DungeonFactory/Infrastructure/DatabaseOptions.cs
+++ DungeonFactory/Infrastructure/DatabaseOptions.cs
@@ -7,19 +7,27 @@ namespace DungeonFactory.Infrastructure
     {
         public static IServiceCollection AddDatabase(this IServiceCollection serviceCollection)
         {
-            return serviceCollection.AddSingleton(services =>
+            return serviceCollection.AddSingleton<ILiteDatabase>(services =>
             {
                 var options = services.GetService<IOptions<DatabaseOptions>>()?.Value ?? new();
 
-                return new LiteDatabase(options.ConnectionString);
+                BsonMapper.Global.EmptyStringToNull = options.EmptyStringToNull;
+
+                var db = new LiteDatabase(options.ConnectionString);
+
+                return db;
             });
         }
     }
 
     public class DatabaseOptions
     {
-        public const string DefaultDatabase = "data.db";
+        public const string DefaultDatabase = "filename=data.db;";
 
         public string ConnectionString { get; set; } = DefaultDatabase;
+
+        public bool EmptyStringToNull { get; set; } = false;
+
+        public bool EnableLogging { get; set; } = true;
     }
 }
