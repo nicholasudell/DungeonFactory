3b935ba8996375561c61e2eecdc29cb28251ca8c
nick udell <nick@udellgames.com>
2022-07-10 13:28:44 +0100

    Nav bar shows a page per page in the DB

--- DungeonFactory/Scripts/Index.js
+++ DungeonFactory/Scripts/Index.js
@@ -1,3 +1,10 @@
-﻿import { initialiseProseMirror } from "./ProseMirror/ProseMirror.js";
+﻿import { initialiseProseMirror, updateProseMirror, destroyProseMirror } from "./ProseMirror/ProseMirror.js";
 
 window.initialiseProseMirror = initialiseProseMirror;
+window.updateProseMirror = updateProseMirror;
+window.destroyProseMirror = destroyProseMirror;
+
+window.initialiseMenu = function () { 
+    $('.sidebar.menu').first()
+        .sidebar('attach events', '#itemCloser');
+};
