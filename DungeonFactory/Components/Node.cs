fac940bfd3814a4ded1463339a723087f5e4df22
nick udell <nick@udellgames.com>
2022-07-25 10:48:49 +0100

    Adds a tree view component for our navigation

---
+++ DungeonFactory/Components/Node.cs
@@ -0,0 +1,69 @@
+﻿using DungeonFactory.Model;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SemanticComponents
+{
+	public class Node : ICollection<Node>
+	{
+		public List<Node> Children { get; set; } = new();
+
+        public bool Expanded { get; set; } = false;
+
+        public Node(Document document)
+        {
+            Document = document;
+
+            foreach(var child in Document.Children)
+            {
+                Children.Add(new Node(child));
+            }
+        }
+
+
+        public int Count => ((ICollection<Node>)Children).Count;
+
+        public bool IsReadOnly => ((ICollection<Node>)Children).IsReadOnly;
+
+        public Document Document { get; }
+
+        public void Add(Node item)
+        {
+            ((ICollection<Node>)Children).Add(item);
+        }
+
+        public void Clear()
+        {
+            ((ICollection<Node>)Children).Clear();
+        }
+
+        public bool Contains(Node item)
+        {
+            return ((ICollection<Node>)Children).Contains(item);
+        }
+
+        public void CopyTo(Node[] array, int arrayIndex)
+        {
+            ((ICollection<Node>)Children).CopyTo(array, arrayIndex);
+        }
+
+        public IEnumerator<Node> GetEnumerator()
+        {
+            return ((IEnumerable<Node>)Children).GetEnumerator();
+        }
+
+        public bool Remove(Node item)
+        {
+            return ((ICollection<Node>)Children).Remove(item);
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return ((IEnumerable)Children).GetEnumerator();
+        }
+    }
+}
---
+++ DungeonFactory/Model/TreeWalker.cs
@@ -0,0 +1,32 @@
+﻿namespace DungeonFactory.Model
+{
+    public class TreeWalker<T> where T: class
+    {
+        private readonly Func<T, IEnumerable<T>> childrenSelector;
+
+        public TreeWalker(Func<T, IEnumerable<T>> childrenSelector)
+        {
+            this.childrenSelector = childrenSelector;
+        }
+
+        public (T? result, bool success) Find(T root, Func<T,bool> predicate)
+        {
+            if(predicate(root))
+            {
+                return (root, true);
+            }
+
+            foreach(var child in childrenSelector(root))
+            {
+                (var result, var success) = Find(child, predicate);
+                
+                if (success)
+                {
+                    return (result, success);
+                }
+            }
+
+            return (default(T), false);
+        }
+    }
+}
